f7ffedca94dc65619e3ab73f36341c2de5ecf97b
Carolina Abreu <anacarolinaks19@gmail.com>
2022-02-07 10:01:12 -0300

    feat: Visualizar usuario

--- backend/shortner/index.js
+++ backend/shortner/index.js
@@ -11,3 +11,20 @@ app.get("/api/user", (request, response) => {
     return response.send(users);
 });
 
+app.get("/api/user/:id", (request, response) => {
+  const id = request.params.id;
+
+  const user = users.find((user) => user.id === id);
+
+  if (user) {
+      return response.status(200).send({ user });
+  }
+
+  return response.status(404).send({ message: "User not exist" });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server Running on PORT 3000");
+});
